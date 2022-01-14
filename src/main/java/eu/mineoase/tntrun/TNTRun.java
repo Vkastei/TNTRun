f33e9479161f8a03ed9d57507c79b3a2eab3fa2f
amazonalexa <62266597+amazonal3xa@users.noreply.github.com>
2022-01-14 19:25:27 +0100

    yeet

---
+++ src/main/java/eu/mineoase/tntrun/TNTRun.java
@@ -0,0 +1,17 @@
+package eu.mineoase.tntrun;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TNTRun extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
