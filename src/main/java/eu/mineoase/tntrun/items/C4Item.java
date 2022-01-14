34f40317d1a1931dd74bcc259a6f1665261c7fb5
amazonalexa <62266597+amazonal3xa@users.noreply.github.com>
2022-01-14 19:33:13 +0100

    final import

--- src/main/java/eu/mineoase/tntrun/items/C4Item.java
+++ src/main/java/eu/mineoase/tntrun/items/C4Item.java
@@ -1,7 +1,31 @@
 package eu.mineoase.tntrun.items;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class C4Item implements Listener {
+    public static int c4Price = 1000;
 
+    public static ItemStack c4(){
+        ItemStack c4 = new ItemStack(Material.REDSTONE_TORCH);
+        ItemMeta c4Meta = c4.getItemMeta();
+        c4Meta.setDisplayName("C4");
+        c4.setItemMeta(c4Meta);
+
+        return c4;
+    }
+    public static ItemStack lever(){
+        ItemStack lever = new ItemStack(Material.LEVER);
+        ItemMeta leverMeta = lever.getItemMeta();
+        leverMeta.setDisplayName("Fernzünder");
+        lever.setItemMeta(leverMeta);
+
+        return lever;
+    }
+
+    public void c4PlaceEvent(PlayerInteractEvent e1);
 }
